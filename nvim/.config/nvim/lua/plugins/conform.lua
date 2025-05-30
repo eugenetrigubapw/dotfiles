8f4c022f37992f990754b2b2ff87b50aacb761d4
Eugene Triguba <eugenetriguba@gmail.com>
2025-05-29 22:01:08 -0400

    add barbar and tweak conform/lsp

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -75,6 +75,8 @@ return {
         javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         css = { 'prettierd', 'prettier', stop_after_first = true },
+        htmldjango = { 'prettierd', 'prettier', stop_after_first = true },
+        html = { 'prettierd', 'prettier', stop_after_first = true },
       },
       formatters = {
         isort = {
