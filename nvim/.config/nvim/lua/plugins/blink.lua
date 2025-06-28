224fb93bb7a5c78b1ae5969dbe075423a49d8991
Eugene Triguba <eugenetriguba@gmail.com>
2025-06-27 22:01:27 -0400

    Add zig to dap, refactor debug config, and add build to blink

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,8 +1,9 @@
 return {
   { -- Autocompletion
     'saghen/blink.cmp',
+    build = 'cargo +nightly build --release',
     event = 'VimEnter',
-    version = '1.*',
+    version = '*',
     dependencies = {
       -- Snippet Engine
       {
