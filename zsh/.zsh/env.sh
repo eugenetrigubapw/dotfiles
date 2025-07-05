9940e18f90a91740dda9d4b01a692a6f02cb01f2
Eugene Triguba <eugenetriguba@gmail.com>
2025-07-04 23:20:25 -0400

    Tidy up zsh config

---
+++ zsh/.zsh/env.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+export EDITOR="nvim"
+export BROWSER="vivaldi"
+export GPG_TTY=$(tty)
+export PAGER="less"
+export MANPAGER="less"
+export BAT_THEME="tokyonight_night"
+export SRC_DIR="$HOME/src"
+export BIN_DIR="$HOME/bin"
---
+++ zsh/.zsh/history.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+export HISTFILE=~/.zhistory
+export HISTSIZE=100000
+export SAVEHIST=100000
