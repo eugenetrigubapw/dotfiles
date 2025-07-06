191f38d626ee4e59c818edbdc54efdd832e07d33
Eugene Triguba <eugenetriguba@gmail.com>
2025-07-06 15:59:55 -0400

    Add oil hotkey

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -4,6 +4,9 @@ return {
   ---@type oil.SetupOpts
   opts = {},
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function(_, opts)
+    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    require('oil').setup(opts)
+  end,
   lazy = false,
 }
