c25d81ecce36f47cef43da0043b82a3853eee708
Eugene Triguba <eugenetriguba@gmail.com>
2025-07-07 07:10:32 -0400

    Add hotkey for oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -5,6 +5,7 @@ return {
   opts = {},
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   config = function(_, opts)
+    vim.keymap.set('n', '\\', '<CMD>Oil --float<CR>', { desc = 'Open parent directory in floating window' })
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
     require('oil').setup(opts)
   end,
