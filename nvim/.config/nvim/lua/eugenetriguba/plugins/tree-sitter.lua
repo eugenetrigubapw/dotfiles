17cf256dfcf32d298ff29115b747df2df678c7bc
Eugene Triguba <hello@eugenetriguba.com>
2025-08-11 22:19:05 -0400

    Revert to basic zsh prompt

--- nvim/.config/nvim/lua/eugenetriguba/plugins/tree-sitter.lua
+++ nvim/.config/nvim/lua/eugenetriguba/plugins/tree-sitter.lua
@@ -2,6 +2,7 @@ vim.pack.add {
   {
     name = 'nvim-treesitter',
     src = 'https://github.com/nvim-treesitter/nvim-treesitter',
+    version = 'main',
   },
   {
     name = 'nvim-treesitter-context',
