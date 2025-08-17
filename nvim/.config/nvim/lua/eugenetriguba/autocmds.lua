b3a301acd6767d8fa9bc73a81be4ae51d18234ce
Eugene Triguba <hello@eugenetriguba.com>
2025-08-17 10:48:17 -0400

    Fix indent configuration with nvim

--- nvim/.config/nvim/lua/eugenetriguba/autocmds.lua
+++ nvim/.config/nvim/lua/eugenetriguba/autocmds.lua
@@ -6,10 +6,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.hl.on_yank()
   end,
 })
-
-vim.api.nvim_create_autocmd('FileType', {
-  callback = function()
-    vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-  end,
-})
