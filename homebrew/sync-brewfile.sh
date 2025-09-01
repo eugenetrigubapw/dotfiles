bd3a72a92a79de7d668e5a92142a114ee8857d6c
Eugene Triguba <hello@eugenetriguba.com>
2025-09-01 13:39:52 -0400

    Update brewfile

--- homebrew/sync-brewfile.sh
+++ homebrew/sync-brewfile.sh
@@ -4,7 +4,8 @@ set -eou pipefail
 # Create a new Brewfile with only manually installed formulae and casks
 (
   echo 'tap "neovim/neovim"'
-  echo 'tap "eugenetriguba/bolt"'
+  echo 'tap "eugenetriguba/bolt", "https://git.sr.ht/~eugenetriguba/homebrew-bolt"'
+  echo 'tap "eugenetriguba/justify", "https://git.sr.ht/~eugenetriguba/homebrew-justify"'
   brew bundle dump --file=- --force |
     (
       grep -E "^brew" | grep -f <(brew leaves)
