5250f8359069808faf7c9001df9cc54ca1ab207a
Eugene Triguba <hello@eugenetriguba.com>
2025-10-12 11:09:05 -0400

    Add java home

--- nvim/.config/nvim/lua/eugenetriguba/global.lua
+++ nvim/.config/nvim/lua/eugenetriguba/global.lua
@@ -6,4 +6,4 @@ vim.g.maplocalleader = ' '
 -- Set Python 3 to a virtual environment with `pynvim` installed
 -- to support python plugins.
 vim.g.python3_host_prog = vim.fn.expand '~/.config/nvim/venv/bin/python'
-
+vim.g.java_home = '/opt/homebrew/opt/openjdk@24'
