5e6bb8c3fa19ae2c8bd42fcf3c052569381cc395
Eugene Triguba <hello@eugenetriguba.com>
2025-11-26 09:05:34 -0500

    Remove delete buffer command

--- nvim/.config/nvim/lua/eugenetriguba/keymaps.lua
+++ nvim/.config/nvim/lua/eugenetriguba/keymaps.lua
@@ -22,13 +22,3 @@ map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-local function confirm_and_delete_buffer()
-  local confirm = vim.fn.confirm('Delete buffer and file?', '&Y\n&N', 2)
-
-  if confirm == 1 then
-    os.remove(vim.fn.expand '%')
-    vim.api.nvim_buf_delete(0, { force = true })
-  end
-end
-
-map('n', '<leader>df', confirm_and_delete_buffer)
