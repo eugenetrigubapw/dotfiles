e7abc890f4293cd333e0e62f3662b569648cdc38
Eugene Triguba <hello@eugenetriguba.com>
2025-11-26 09:05:09 -0500

    Remove custom python/java venv/home settings

--- nvim/.config/nvim/lua/eugenetriguba/global.lua
+++ nvim/.config/nvim/lua/eugenetriguba/global.lua
@@ -2,8 +2,3 @@
 -- See `:help mapleader`
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
--- Set Python 3 to a virtual environment with `pynvim` installed
--- to support python plugins.
-vim.g.python3_host_prog = vim.fn.expand '~/.config/nvim/venv/bin/python'
-vim.g.java_home = '/opt/homebrew/opt/openjdk@24'
