e1e40a1505fff0f4dafa6706b3c457554842a2c9
Eugene Triguba <hello@eugenetriguba.com>
2025-11-27 20:27:30 -0500

    Update packages

--- pkg/update-openbsd-packages.sh
+++ pkg/update-openbsd-packages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-pkg_info -m | awk '{ print  }' | grep -v 'firmware' | sed 's/-[0-9].*//' >openbsd-packages.txt
+pkg_info -m | awk '{ print  }' | grep -v 'firmware' | sed 's/-[0-9].*//' | sort >openbsd-packages.txt
